59ee744b16f179e57a2e612629112d269d161646
HuAnYi9527 <59110106+HuAnYi9527@users.noreply.github.com>
2022-03-24 16:20:50 +0800

    Update ioT.py

--- ioT.py
+++ ioT.py
@@ -1,1 +1,27 @@
-print("hello ioT")
+import RPi.GPIO as GPIO
+import time
+
+buttonup=True
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(23, GPIO.IN,pull_up_down = GPIO.PUD_UP)
+GPIO.setup(18, GPIO.OUT)
+GPIO.output(18,0)
+GPIO.setup(16, GPIO.OUT)
+GPIO.output(16,0)
+
+while True:
+    
+    if(GPIO.input(16)!=True):
+            
+        GPIO.output(16,1)
+        time.sleep(5)
+    
+    GPIO.output(16,0)
+        
+
+
+
+
+
+
